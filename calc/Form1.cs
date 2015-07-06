9cf554afc76ec11f61fcd365bbb6f1848069b032
Elena <elena_efremova97@mail.ru>
2015-07-06 14:48:31 +0600

    Base functions

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -12,34 +12,81 @@ namespace calc
 {
     public partial class Form1 : Form
     {
+        public double ValOne;
+        public double ValTwo;
+        public double Result;
+
+        
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void InputLable_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void InpValOne_TextChanged(object sender, EventArgs e)
         {
+            string input = InpValOne.Text;
+            if (input.Length == 0)
+                this.ValOne = 0;
+            else
+                this.ValOne =  Convert.ToDouble(input);
+        }
+
+        private void InpValTwo_TextChanged(object sender, EventArgs e)
+        {
+            string input = InpValTwo.Text;
+            if (input.Length == 0)
+                this.ValTwo = 0;
+            else
+                this.ValTwo = Convert.ToDouble(input);
+        }
+
+        private void Output_TextChanged(object sender, EventArgs e)
+        {
+            //Output.Text=
+        }
+
+        private void ButtMltp_Click(object sender, EventArgs e)
+        {
+            this.Result = this.ValOne * this.ValTwo;
+            Output.Text = this.Result.ToString();
+            this.Reset();
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void ButtDiv_Click(object sender, EventArgs e)
         {
+            this.Result = this.ValOne  / this.ValTwo;
+            Output.Text = this.Result.ToString();
+            this.Reset();
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ButtAdd_Click(object sender, EventArgs e)
         {
+            this.Result = this.ValOne + this.ValTwo;
+            Output.Text = this.Result.ToString();
+            this.Reset();
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void ButtSubr_Click(object sender, EventArgs e)
         {
-
+            this.Result = this.ValOne - this.ValTwo;
+            Output.Text = this.Result.ToString();
+            this.Reset();
         }
+
+        public void Reset()
+        {
+            InpValOne.Text = "";
+            InpValTwo.Text = "";
+        }
+
+       
     }
 }
