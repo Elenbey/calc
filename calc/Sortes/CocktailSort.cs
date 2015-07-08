0ca4c8acc7d7a89edfa7cb4462fc3e8c72a8393a
Elena <elena_efremova97@mail.ru>
2015-07-08 15:32:03 +0600

    add CoctailSort and SelectionSort

--- calc/Sortes/CocktailSort.cs
+++ calc/Sortes/CocktailSort.cs
@@ -4,7 +4,37 @@
     {
         public int[] Sort(int[] array)
         {
-            
+            for (int i = 0; i < array.Length/2; i++)
+            {
+                bool swapped = false;
+                for (int j = i; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+                for (int j = array.Length - 2 - i; j > i; j--)
+                {
+                    if (array[j] > array[j - 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = tmp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+            return array;
         }
     }
 }
+
+
--- calc/Sortes/SelectionSort.cs
+++ calc/Sortes/SelectionSort.cs
@@ -4,7 +4,26 @@
     {
         public int[] Sort(int[] array)
         {
-            
+            int posmin, temp;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                posmin = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[posmin])
+                    {    
+                        posmin = j;
+                    }
+                }
+                    if (posmin != i)
+                {
+                    temp = array[i];
+                    array[i] = array[posmin];
+                    array[posmin] = temp;
+                }
+            }
+            return array;
         }
     }
 }
