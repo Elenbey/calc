c5f7d711c4beab226b7c51cb83e4626d0c677ba8
Maxbey <maximbeiner@gmail.com>
2015-07-08 14:55:58 +0600

    add test case for SingleArgumentFactory

---
+++ calc.Tests/FactoriesTests/SingleArgumentFactoryTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using calc.SingleArgumentCalcs;
+using NUnit.Framework;
+
+namespace calc.Factories
+{
+    [TestFixture]
+    public  class SingleArgumentFactoryTest
+    {
+        [TestCase("Module", typeof(ModuleCalculator))]
+        [TestCase("Cos", typeof(CosCalculator))]
+        [TestCase("Sin", typeof(SinCalculator))]
+        [TestCase("Sqrt", typeof(SqrtCalculator))]
+        [TestCase("Tan", typeof(TanCalculator))]
+        [TestCase("Log", typeof(LogCalculator))]
+        [TestCase("Sinh", typeof(SinhCalculator))]
+
+        public  void CreateCalculatorTest(string calculatorName, Type expectedCalculator)
+        {
+            var calculator = SingleArgumentFactory.CreateCalculator(calculatorName);
+
+            Assert.AreEqual(calculator.GetType(), expectedCalculator);
+            
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void UnknownCalculatorTest()
+        {
+            var calculator = SingleArgumentFactory.CreateCalculator("wrong name");
+        }
+    }
+}
---
+++ calc.Tests/FactoriesTests/TwoArgumentsFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using calc.TwoArgumentsCalculator;
+
+namespace calc.Factories
+{
+    public static class TwoArgumentsFactory
+    {
+        public static ITwoArgumentsCalculator CreateCalculator(string calculatorName)
+        {
+            switch (calculatorName)
+            {
+                case "Addition":
+                    return new AdditionCalculator();
+
+                case "Division":
+                    return new DivisionCalculator();
+
+                case "Multiply":
+                    return new MultiplyCalculator();
+
+                case "Subtraction":
+                    return new SubtractionCalculator();
+
+                case "Power":
+                    return new PowerCalculator();
+                default:
+                    throw new Exception("Unknown calculator");
+            }
+        }
+    }
+}
