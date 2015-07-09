c08650ff7ea225c98ac39b5b1ad92567bce37b32
Maxbey <maximbeiner@gmail.com>
2015-07-09 11:54:15 +0600

    add unit tests for ArrayHelpers

---
+++ calc.Tests/HelpersTests/ArrayHelpersTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace calc.Helpers
+{
+    [TestFixture]
+    public  class ArrayHelpersTest
+    {
+        [TestCase("1 2 3 4 5", ' ', new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase("-5", ' ', new int[] { -5 })]
+
+        public void StringToArrayTest(string toSplit,char delimiter, int[] result)
+        {
+
+            var testResult = ArrayHelpers.StringToArray(toSplit, delimiter);
+            Assert.AreEqual(testResult, result);
+        }
+
+        [TestCase(new int[] {1, 2, 3, 4, 5}, ',', "1,2,3,4,5")]
+        [TestCase(new int[] { -5 }, ' ', "-5")]
+        public void ArrayToStringTest(int[] array, char delimiter, string result)
+        {
+            var testResult = ArrayHelpers.ArrayToString(array, delimiter);
+            Assert.AreEqual(testResult, result);
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void EmptyStringToArrayTest()
+        {
+            ArrayHelpers.StringToArray("", ',');
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void EmptyArrayToStringTest()
+        {
+            ArrayHelpers.ArrayToString(new int[]{}, ',');
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CrushedStringToArrayTest()
+        {
+            ArrayHelpers.StringToArray("1,,,,4", ',');
+        }
+
+    }
+}
--- calc/Helpers/ArrayHelpers.cs
+++ calc/Helpers/ArrayHelpers.cs
@@ -7,24 +7,43 @@ namespace calc.Helpers
     {
         public static int[] StringToArray(string toSplit,char delimiter)
         {
+            if (toSplit.Length == 0)
+            {
+                throw new Exception("sent an empty string");
+            }
             
             string[] stringItems = toSplit.Split(delimiter);
             int[] result = new int[stringItems.Length];
 
             for (int i = 0; i < stringItems.Length; i++)
             {
-                result[i] = Convert.ToInt32(stringItems[i]);
+                var item = stringItems[i];
+
+                if (item.Length == 0)
+                {
+                    throw new Exception("sent an crushed string");
+                }
+
+                result[i] = Convert.ToInt32(item);
             }
             return result;
         }
 
         public static String ArrayToString(int[] array, char delimiter)
         {
-           String result = "";
+            if (array.Length == 0)
+            {
+                throw new Exception("sent an empty array");
+            }
+            String result = "";
 
             for (int i = 0; i < array.Length; i++)
             {
-                 result += array[i].ToString() + delimiter;
+                result += array[i].ToString();
+                if (i < array.Length - 1)
+                {
+                    result += delimiter;
+                }
             }
 
             return result;
